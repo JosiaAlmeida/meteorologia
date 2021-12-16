7698f81623e706b0788136f75a2a9c43e0a8e5b0
Josia Almeida <josiaalmeida0001@gmail.com>
2021-12-16 20:48:31 +0000

    Testando requisicao

--- src/clients/_test_/stormGlass.test.ts
+++ src/clients/_test_/stormGlass.test.ts
@@ -1,11 +1,16 @@
 import {StormGlass} from './stormGlass'
+import axios from 'axios'
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3hours.json'
 
+jest.mock('axios')
 describe('StormGlass client',()=>{
     it('should return the normalized forecast from the StormGlass service', async()=>{
         const lat =-33.792726
         const lng = 151.289824
 
-        const stormGlass = new StormGlass()
+        axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture)
+
+        const stormGlass = new StormGlass(axios)
         const response = await stormGlass.fetchPoints(lat,lng)
         expect(response).toEqual({})
     })
--- src/clients/_test_/stormGlass.ts
+++ src/clients/_test_/stormGlass.ts
@@ -1,5 +1,15 @@
+import { AxiosStatic } from "axios";
+
 export class StormGlass{
+    readonly stormGlassAPIParams = 'swellDorection,swellHeight,swellPeriod,waveDorectopm,waveHeight,windDirection,windSpeed'
+    readonly stormGlassAPISource = 'noaaa'
+
+    constructor(protected request: AxiosStatic){}
+
     public async fetchPoints(lat: number, lng: number):Promise<{}> {
+        this.request.get(
+            `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`
+        )
         return Promise.resolve({})
     }
 }
